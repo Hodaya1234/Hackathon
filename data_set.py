04a70c2a700f2452410cb724d1581a4b65d85b18
Hodaya <ko.hodaya@gmail.com>
2019-05-29 21:22:49 +0300

    finished writing without debugging

--- data_set.py
+++ data_set.py
@@ -6,6 +6,7 @@ class DataSet(data_utils.Dataset):
         super(DataSet, self).__init__()
         self.all_x = torch.from_numpy(x)
         self.all_y = torch.from_numpy(y)
+        self.all_y = self.all_y.long()
         self.n_data = len(y)
 
     def __getitem__(self, index):
